6a8ef5da1716a546cdba25c9d7671326888cdd59
juszkn <justasze@yahoo.com>
2024-02-02 16:55:15 +0200

    masyvai uzduotys 2 dalis

--- javascripts/02V/masyvuUzduotys.js
+++ javascripts/02V/masyvuUzduotys.js
@@ -4,7 +4,7 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-document.write('<h3>Prima uzduotis</h3>')
+document.write('<h3>Prima užduotis</h3>')
 //Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), 
 // kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
 
@@ -15,7 +15,7 @@ for (let i = 0; i < 30; i++) {
 
 document.write(randNumArray);
 
-document.write('<h3>Antra uzduotis</h3>')
+document.write('<h3>Antra užduotis</h3>')
 document.write('<h3>a)</h3>')
 //Naudodamiesi 1 uždavinio masyvu:
 // Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
@@ -128,3 +128,97 @@ for (let i = 0; i < randNumArray.length; i++) {
     }
 }
 document.write(randNumArray);
+
+
+document.write(`<h3>Trečia užduotis</h3>`);
+// Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai
+// nuo 100 iki 999. Masyvų ilgiai 100. 
+// Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
+
+const array_1= [];
+const array_2= [];
+
+for (let i = 0; i < 100; i++) {
+    if(i === 0) {
+        array_1[i] = rand(100, 999);
+    } else {
+        let randomnumber = rand(100, 999);
+        if(array_1.includes(randomnumber)) {
+            i--;
+        } else {
+            array_1.push(randomnumber);
+        }
+    }
+}
+for (let i = 0; i < 100; i++) {
+    if(i === 0) {
+        array_2[i] = rand(100, 999);
+    } else {
+        let randomnumber = rand(100, 999);
+        if(array_2.includes(randomnumber)) {
+            i--;
+        } else {
+            array_2.push(randomnumber);
+        }
+    }
+}
+document.writeln(`${array_1}`);
+// document.writeln(`${array_1.length}`);
+
+document.write(`${array_2}`);
+// document.writeln(`${array_2.length}`);
+
+
+document.write(`<h3>Ketvirta užduotis</h3>`);
+// Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra 
+// pirmame 3 uždavinio masyve,
+// bet nėra antrame 3 uždavinio masyve
+
+const array_3 = [];
+let isSame = false;
+
+for(let i = 0; i < array_1.length; i++) {
+    // console.log("Pirmas masyvas:" + array_1[i])
+    for(let j = 0; j < array_2.length; j++) {
+        // console.log("Antras masyvas: " + array_2[j])
+        if(array_1[i] === array_2[j]) {
+            isSame = true;
+            break;
+        }
+    } 
+        if (isSame === false) {
+            array_3.push(array_1[i]);
+    } 
+    isSame = false;
+}
+document.write(array_3);
+
+
+document.write(`<h3>Penkta užduotis</h3>`);
+// Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra 
+// pirmame 3 uždavinio masyve,
+// bet nėra antrame 3 uždavinio masyve
+
+const fourthArray = [];
+for (let i = 0; i < array_1.length; i++) {
+    for (let j = 0; j < array_2.length; j++) {
+        if (array_1[i] === array_2[j]) {
+            fourthArray.push(array_1[i])
+        }
+    }
+}
+document.write(fourthArray);
+
+document.write(`<h3>Šešta užduotis</h3>`);
+//Sugeneruokite 10 skaičių masyvą pagal taisyklę: Du pirmi skaičiai- 
+// atsitiktiniai nuo 5 iki 25. Trečias, pirmo ir antro suma. 
+// Ketvirtas- antro ir trečio suma.
+// Penktas trečio ir ketvirto suma ir t.t.
+
+const unoArray = [rand(5, 25), rand(5, 25)];
+
+for(let i = 2; i < 10; i++) {
+    unoArray[i] = unoArray[i - 1] + unoArray[i - 2];
+}
+
+document.write(unoArray);
